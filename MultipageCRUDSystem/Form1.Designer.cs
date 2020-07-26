96c3ceed204afb01f6fd9ab5560b4b1ca5fe7a7a
ben90241 <ben90241@gmail.com>
2020-07-26 03:28:28 -0700

    Add project files.

---
+++ MultipageCRUDSystem/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace MultipageCRUDSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Branch = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.studentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new MultipageCRUDSystem.Database1DataSet();
+            this.studentTableAdapter = new MultipageCRUDSystem.Database1DataSetTableAdapters.StudentTableAdapter();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(-2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Multipage CRUD System";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Student Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(81, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(81, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Sem/ Year";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(81, 388);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Branch";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(2, 455);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(189, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Search By Student Name";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Blue;
+            this.label8.Location = new System.Drawing.Point(210, 72);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(200, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Student Information";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(215, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(215, 186);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(257, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(215, 250);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(257, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(215, 318);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(257, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Computer Science",
+            "Electrical Engineering",
+            "Mechanical Engineering",
+            "Chemical Engineering",
+            "Civil",
+            "Finance",
+            "English"});
+            this.comboBox1.Location = new System.Drawing.Point(215, 385);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(257, 24);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(215, 452);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(257, 22);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(498, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 53);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "New/ Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(498, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 58);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(498, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 52);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(498, 368);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 57);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.SName,
+            this.Address,
+            this.Phone,
+            this.Sem,
+            this.Branch});
+            this.dataGridView1.Location = new System.Drawing.Point(655, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(686, 391);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // SName
+            // 
+            this.SName.HeaderText = "SName";
+            this.SName.MinimumWidth = 6;
+            this.SName.Name = "SName";
+            this.SName.Width = 125;
+            // 
+            // Address
+            // 
+            this.Address.HeaderText = "Address";
+            this.Address.MinimumWidth = 6;
+            this.Address.Name = "Address";
+            this.Address.Width = 125;
+            // 
+            // Phone
+            // 
+            this.Phone.HeaderText = "Phone";
+            this.Phone.MinimumWidth = 6;
+            this.Phone.Name = "Phone";
+            this.Phone.Width = 125;
+            // 
+            // Sem
+            // 
+            this.Sem.HeaderText = "Sem";
+            this.Sem.MinimumWidth = 6;
+            this.Sem.Name = "Sem";
+            this.Sem.Width = 125;
+            // 
+            // Branch
+            // 
+            this.Branch.HeaderText = "Branch";
+            this.Branch.MinimumWidth = 6;
+            this.Branch.Name = "Branch";
+            this.Branch.Width = 125;
+            // 
+            // studentBindingSource
+            // 
+            this.studentBindingSource.DataMember = "Student";
+            this.studentBindingSource.DataSource = this.database1DataSet;
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // studentTableAdapter
+            // 
+            this.studentTableAdapter.ClearBeforeFill = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(498, 452);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 57);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Reload";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1353, 514);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource studentBindingSource;
+        private Database1DataSetTableAdapters.StudentTableAdapter studentTableAdapter;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Branch;
+    }
+}
+
---
+++ MultipageCRUDSystem/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultipageCRUDSystem
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Benson\source\repos\MultipageCRUDSystem\MultipageCRUDSystem\Database1.mdf;Integrated Security=True");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Student' table. You can move, or remove it, as needed.
+            //this.studentTableAdapter.Fill(this.database1DataSet.Student);
+            reload();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            comboBox1.SelectedIndex = -1;
+        }
+        private void reload()
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("select * from Student", sqlCon);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                //dataGridView1.DataSource = dt;
+                dataGridView1.Rows.Clear();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    int i = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[0].Value = dr[0].ToString();
+                    dataGridView1.Rows[i].Cells[1].Value = dr[1].ToString();
+                    dataGridView1.Rows[i].Cells[2].Value = dr[2].ToString();
+                    dataGridView1.Rows[i].Cells[3].Value = dr[3].ToString();
+                    dataGridView1.Rows[i].Cells[4].Value = dr[4].ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //insert
+        {
+            try
+            {
+                sqlCon.Open();
+                SqlCommand cmd = new SqlCommand("if not exists (select sname from Student where sname=@sname) " +
+                "begin insert into Student (sname, sadd, phone, sem, branch) values (@sname, @sadd, @phone, @sem, @branch) end", sqlCon);
+                //SqlCommand cmd = new SqlCommand("begin insert into Student (sname, sadd, phone, sem, branch) values (@sname, @sadd, @phone, @sem, @branch) end", sqlCon);
+
+                cmd.Parameters.AddWithValue("@sname", textBox1.Text);
+                cmd.Parameters.AddWithValue("@sadd", textBox2.Text);
+                cmd.Parameters.AddWithValue("@phone", int.Parse(textBox3.Text));
+                cmd.Parameters.AddWithValue("@sem", int.Parse(textBox4.Text));
+                cmd.Parameters.AddWithValue("@branch", comboBox1.SelectedItem.ToString());
+
+                int i = cmd.ExecuteNonQuery();
+
+                if(i>=1)
+                {
+                    MessageBox.Show(i+" Student Registered Successfully : " + textBox1.Text.ToString());
+                }
+                else
+                {
+                    MessageBox.Show(" Student is not Registered: ");
+                }
+
+                //string str = String.Format("Name: {0}, Address: {1}, Phone: {2}, Semester: {3}, Branch: {4}", textBox1.Text.ToString(), textBox2.Text.ToString(), textBox3.Text.ToString(), textBox4.Text.ToString(), comboBox1.SelectedItem.ToString());
+                //MessageBox.Show(str);
+
+                reload();
+
+                sqlCon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(" Error is "+ ex.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //update
+        {
+            try 
+            {
+                sqlCon.Open();
+                SqlCommand cmd = new SqlCommand("update Student set sadd=@sadd, phone=@phone, sem=@sem, branch=@branch where sname=@sname", sqlCon);
+
+                cmd.Parameters.AddWithValue("@sname", textBox1.Text);
+                cmd.Parameters.AddWithValue("@sadd", textBox2.Text);
+                cmd.Parameters.AddWithValue("@phone", int.Parse(textBox3.Text));
+                cmd.Parameters.AddWithValue("@sem", int.Parse(textBox4.Text));
+                cmd.Parameters.AddWithValue("@branch", comboBox1.SelectedItem.ToString());
+
+                int i = cmd.ExecuteNonQuery();
+
+                reload();
+
+                sqlCon.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) //delete
+        {
+            try
+            {
+                sqlCon.Open();
+
+                SqlCommand cmd = new SqlCommand("delete Student where sname=@sname", sqlCon);
+
+                cmd.Parameters.AddWithValue("@sname", textBox1.Text);
+
+                int i = cmd.ExecuteNonQuery();
+
+                reload();
+
+                sqlCon.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            reload();
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+                textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+                textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+                textBox4.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+                comboBox1.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlCon.Open();
+
+                SqlCommand cmd = new SqlCommand("select * from Student where sname like '%"+textBox5.Text.ToString()+"%'", sqlCon);
+
+                //SqlCommand cmd = new SqlCommand("select * from Student where sname like '%@sname%'", sqlCon);
+                //cmd.Parameters.AddWithValue("@sname", textBox5.Text);
+
+                //int i = cmd.ExecuteNonQuery();
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                //dataGridView1.DataSource = dt;
+                dataGridView1.Rows.Clear();
+                foreach (DataRow dr in dt.Rows)
+                {
+                    int i = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[0].Value = dr[0].ToString();
+                    dataGridView1.Rows[i].Cells[1].Value = dr[1].ToString();
+                    dataGridView1.Rows[i].Cells[2].Value = dr[2].ToString();
+                    dataGridView1.Rows[i].Cells[3].Value = dr[3].ToString();
+                    dataGridView1.Rows[i].Cells[4].Value = dr[4].ToString();
+                }
+
+                sqlCon.Close();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
